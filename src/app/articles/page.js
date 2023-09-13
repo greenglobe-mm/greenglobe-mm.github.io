e6eaea9f53fe03695b3b2a29778fc58c633fe952
pmpSalween <pmoepine@gmail.com>
2023-09-13 17:42:20 +0700

    ui for home page

--- src/app/articles/page.js
+++ src/app/articles/page.js
@@ -1,3 +1,8 @@
+import { Suspense } from "react";
+import Loading from "./loading";
+
 export default function Page() {
-    return <h1>Hello, Articles!</h1>
+    return <Suspense fallback={<Loading />}>
+    <h1>Hello, Articles!</h1>
+  </Suspense>
   }
--- src/app/page.js
+++ src/app/page.js
@@ -5,13 +5,9 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          The more the farmers, the greener the globe.
-          {/* <code className="font-mono font-bold">src/app/page.js</code> */}
+        <p className="italic fixed left-0 top-0 flex w-full justify-center lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-green-950 lg:p-4 lg:dark:bg-green-800/30 font-mono font-bold bg-gradient-to-r from-green-950 to-green-700 text-white px-2">
+          Better Life For Farmers
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          By PMP
-        </div>
       </div>
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-green-50 after:via-green-50 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-green-100 before:dark:opacity-10 after:dark:from-green-200 after:dark:via-[#005533] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
@@ -24,6 +20,10 @@ export default function Home() {
           priority
         />
       </div>
+
+      <p className="m-0 flex text-base font-medium text-gray-300">
+        Green Globe Company Limited was founded in 2022. Our company is still young but is up for the challenges.
+      </p>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <Link
