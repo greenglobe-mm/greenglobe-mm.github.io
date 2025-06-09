b3bd8c6cb5765e68b54954569ad1e1a40ea57971
pmpSalween <pmoepine@gmail.com>
2025-06-09 13:56:12 +0700

    clean

--- resources/articleInfo.js
+++
@@ -1,6 +0,0 @@
-const info = {
-    "numberOfFiles" : 1,
-    "otherInfoForFuture": null
-};
-
-export default info;
--- src/app/products/page.js
+++
@@ -1,7 +0,0 @@
-export default function Page() {
-  return (
-    <main className="flex min-h-screen flex-col items-center p-24">
-      <h1>Products!</h1>
-    </main>
-  );
-}
